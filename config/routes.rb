8e889f197d3aac5f7cd54ae55c21f14f9087ce15
grandmasterspoc <dilloncortez@gmail.com>
2015-03-12 17:16:50 -0600

    Finished routes and layout.. for now

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   get 'users/new'
 
   get 'contact' => 'static_pages#contact'
-
   root 'static_pages#home'
-
   get 'home' => 'static_pages#home'
-
   get 'help' => 'static_pages#help'
-
   get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 
   
 
