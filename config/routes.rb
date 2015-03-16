aa4bc0e4b9f2596dca3e95c82a196c0fd3b17a82
grandmasterspoc <dilloncortez@gmail.com>
2015-03-15 19:28:20 -0600

    created error_messages file and integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
-
+  resources :users
   
 
   # The priority is based upon order of creation: first created -> highest priority.
